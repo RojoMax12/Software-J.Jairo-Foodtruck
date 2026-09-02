a0376da2c977335eb942990cf6ca7956f2ec3e04
Johan <johan.neira@usach.cl>
2026-09-01 21:20:58 -0400

    FIX: Name

--- backend-foodtruck/app/Models/Detalle_pedido_Ingrediente.php
+++ backend-foodtruck/app/Models/Detalle_pedido_Ingrediente.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Detalle_pedido_ingrediente extends Model
+class Detalle_pedido_Ingrediente extends Model
 {
     use HasFactory;
 
